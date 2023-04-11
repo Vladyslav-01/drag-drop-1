9a39a480f729af8d1ec4a8402298274935a2870c
Vladyslav-01 <83462359+Vladyslav-01@users.noreply.github.com>
2023-04-11 17:54:11 +0300

    release  1.0

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const dropArea = document.querySelector('.drop-area');
 const text = document.querySelector('.drop-area__text');
 const input = document.querySelector('#link');
+const label = document.querySelector('#label');
 const imgDestination = document.querySelector('.drop-area__loaded-picture');
+const closeButton = document.querySelector('.drop-area__close-btn');
 let dropClass = /drop-area/;
 let supportedExtensions = ['image/jpeg', 'image/jpg', 'image/png']; 
 
@@ -40,23 +42,43 @@ dropArea.addEventListener('drop', (e) => {
 // input logics
 
 input.addEventListener('change', function() {
+    console.log(1);
     let file = this.files[0];
     showFile(file);
 })
 
 // file processor
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('imgURL')) {
+        imgDestination.classList.remove('d-none');
+        closeButton.classList.remove('d-none');
+        imgDestination.src = localStorage.getItem('imgURL');
+    }
+})
+
+closeButton.addEventListener('click', () => {
+    localStorage.removeItem('imgURL');
+    imgDestination.classList.add('d-none');
+    closeButton.classList.add('d-none');
+});
+
 function showFile(file) {
     if (supportedExtensions.includes(file.type)) {
         let reader = new FileReader();
         reader.readAsDataURL(file);
-
+        
         reader.onload = () => {
             let res = reader.result;
-            imgDestination.classList.remove('d-none');
+            localStorage.setItem('imgURL', res);
             imgDestination.src = res;
         }
+
+        imgDestination.classList.remove('d-none');
+        closeButton.classList.remove('d-none');
+        text.removeAttribute('style');
     } else {
-        alert('This file extention is not supported!');
+        text.innerHTML = 'This file is not supported!';
+        text.setAttribute('style', 'color: red');
     }
 }
